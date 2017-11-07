9335f43f60f310fd7b168831bc4a7359904036a0
Tech-A29 <Tech-A29@Tech-A29s-MacBook-Air.local>
2017-11-07 15:47:01 +0000

    can move a piece

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ $( document ).ready(function() {
   var black = false;
   var current = [];
   var enemy = [];
+  var pos = [];
 
   // $(".container").hide();
   $("#play").click(revealBoard);
@@ -22,12 +23,23 @@ $( document ).ready(function() {
   $(".piece").click(sq_and_piece);
   $("td").click(move);
 
+  // function check(){
+  //   debugger
+  //   //if info in array call move
+  //   if(current.length != 0){
+  //     move();
+  //   }
+  // }
 
   function move(){
-    $(this).addClass("piece").attr("id", current[0]);
-    current = [];
+    //debugger
+    $(this).addClass("piece")
+    $(this).attr("id", current[0]);
+    var $previousPiece = $("td")[pos[0]];
+    $($previousPiece).removeClass("piece");
+
+
     // $(this).removeAttr("id");
-    changeTurn();
     // if(unit){
     //   if(!($(unit).indexOf(char))){
     //     $(this).addClass("piece").attr("id", unit);
@@ -41,7 +53,8 @@ $( document ).ready(function() {
   function sq_and_piece(){ //get info of square and piece
     var $td = $(this).attr("data-num");
     var $piece = $(this).attr("id");
-    current.push($tr, $td, $piece);
+    current.push($piece);
+    pos.push($td);
     console.log($td);
     console.log($piece);
   }
